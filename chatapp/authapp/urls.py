1dd00f110f24c3e1ce27a93305262a89b817280e
kamdem Brice <kamdembrice770@gmail.com>
2025-08-10 12:30:35 +0100

    add register page

--- chatapp/authapp/urls.py
+++ chatapp/authapp/urls.py
@@ -3,4 +3,5 @@ from authapp import views
 
 urlpatterns = [
     path('login/', views.login_user, name='login_user'),
+    path('register/', views.register_user, name='register_user'),
 ]
--- chatapp/authapp/views.py
+++ chatapp/authapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_user(request):
     return render(request, 'authapp/login.html')
+
+def register_user(request):
+    return render(request, 'authapp/register.html')
