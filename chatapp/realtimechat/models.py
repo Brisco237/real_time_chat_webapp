2b51e1b5bfab53137efaa0fa9d88a3968bb6ae1f
kamdem Brice <kamdembrice770@gmail.com>
2025-08-12 22:47:51 +0100

    modif models.urls de realtimeapp

--- chatapp/realtimechat/models.py
+++ chatapp/realtimechat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from authapp.models import User
 
 # Create your models here.
+class ChatGroup(models.Model):
+    group_name = models.Charfield(max_length=10000, unique=True)
+
+    def __str__(self):
+        return self.group_name
+
+    class Meta:
+        verbose_name = "ChatGroup"
+        verbose_name_plural = "ChatGroups"
+
+class GroupMessage(models.Model):
+    group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.Charfield(max_length=500)
+    created = models.DateTimeFielsd(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username} : {self.body}'
+
+    class Meta:
+        ordering = ['-created']
