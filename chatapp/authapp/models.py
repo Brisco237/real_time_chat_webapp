e33b2d6fc7d52a846ab8030dbe914408a6d39032
kamdem Brice <kamdembrice770@gmail.com>
2025-08-13 22:41:24 +0100

    add profile_image in model user

--- chatapp/authapp/models.py
+++ chatapp/authapp/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    photo_profile = models.ImageField(upload_to="profile_users/", blank=True, null=True)
+
     def __str__(self):
         return self.username
 
